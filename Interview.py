bc7419bb49481425bed8d91a32bd12f989dcdc60
Swaroop Akkineni <swa7@pitt.edu>
2019-03-20 16:31:23 -0400

    can calculate distance of two points

--- Interview.py
+++ Interview.py
@@ -1,10 +1,23 @@
 import json
+from math import sin, cos, radians, acos
+
+def findDistanceFromOffice(latitude, longitude):
+    dublinRadiansLatitude = radians(float(53.339428))
+    dublinRadiansLongitude = radians(float(-6.257664))
+    personRadiansLatitude = radians(float(latitude))
+    personRadiansLongitude = radians(float(longitude))
+    radiusOfEarth = 6371
+
+    dist = radiusOfEarth * acos(sin(dublinRadiansLatitude)*sin(personRadiansLatitude) + cos(dublinRadiansLatitude)*cos(personRadiansLatitude)*cos(dublinRadiansLongitude - personRadiansLongitude))
+    return dist
 
 def main():
     with open('customers.txt') as file:
         line = file.readline()
         while line:
-            print(line)
+            j = json.loads(line)
+            dist = findDistanceFromOffice(j['latitude'], j['longitude'])
+            print dist
             line = file.readline()
 
 if __name__ == "__main__": main()
