d4661d3e79411062da433cad9cb01de727a95991
Swaroop Akkineni <swa7@pitt.edu>
2019-03-21 23:01:41 -0400

    blurb working and python script looks good

--- Interview.py
+++ Interview.py
@@ -1,28 +1,44 @@
+import sys
 import json
+import os.path
 from math import sin, cos, radians, acos
 
-def findDistanceFromOffice(latitude, longitude):
-    dublinRadiansLatitude = radians(float(53.339428))
-    dublinRadiansLongitude = radians(float(-6.257664))
-    personRadiansLatitude = radians(float(latitude))
-    personRadiansLongitude = radians(float(longitude))
-    radiusOfEarth = 6371
+class Interview:
+    def findDistanceFromOffice(self, latitude, longitude):
+        dublinRadiansLatitude = radians(float(53.339428))
+        dublinRadiansLongitude = radians(float(-6.257664))
+        personRadiansLatitude = radians(float(latitude))
+        personRadiansLongitude = radians(float(longitude))
+        radiusOfEarth = 6371
 
-    dist = radiusOfEarth * acos(sin(dublinRadiansLatitude)*sin(personRadiansLatitude) + cos(dublinRadiansLatitude)*cos(personRadiansLatitude)*cos(dublinRadiansLongitude - personRadiansLongitude))
-    return dist
+        dist = radiusOfEarth * acos(sin(dublinRadiansLatitude)*sin(personRadiansLatitude) + cos(dublinRadiansLatitude)*cos(personRadiansLatitude)*cos(dublinRadiansLongitude - personRadiansLongitude))
+        return dist
+
+    def readFile(self, fileName):
+        dictionary = []
+        with open(fileName) as file:
+            line = file.readline()
+            while line:
+                j = json.loads(line)
+                dist = self.findDistanceFromOffice(j['latitude'], j['longitude'])
+                if(dist < 100):
+                    dictionary.append([j['user_id'], dist])
+                line = file.readline()
+        dictionary.sort(key=lambda x: x[0])
+        return dictionary
+
+    def process(self, file):
+        if os.path.exists(file):
+            dictionary = self.readFile(file)
+            print 'user_id', ' ', 'distance'
+            for user_id, distance in dictionary:
+                print user_id, '       ', distance
+        else:
+            print 'Bad File name'
 
 def main():
-    dictionary = []
-    with open('customers.txt') as file:
-        line = file.readline()
-        while line:
-            j = json.loads(line)
-            dist = findDistanceFromOffice(j['latitude'], j['longitude'])
-            if(dist < 100):
-                dictionary.append([j['user_id'], dist])
-            line = file.readline()
-    dictionary.sort(key=lambda x: x[0])
-    print dictionary
+    interview = Interview();
+    interview.process(sys.argv[1])
 
 
 if __name__ == "__main__": main()
